9da1f964d07cae28d08d8971d9cdd7a0df1d2da4
Ajfury1 <ajfury1@gmail.com>
2024-04-11 19:18:10 +0530

    home page background and navigation section added

--- src/pages/HeaderSection.vue
+++ src/pages/HeaderSection.vue
@@ -1,27 +1,40 @@
 <template>
-    <div class="relative w-full h-fit-content bg-no-repeat bg-fixed" style="background-image: url('src/assets/images/BG_Sample-1.png'); background-size: 100% 100%;">
-        <!-- Navigation Bar -->
-        <!-- <Navigation class="absolute top-5 left-0 right-0 z-10"/> -->
+    <div class="w-full h-fit-content bg-no-repeat bg-fixed m-0" style="background-image: url('src/assets/images/BG_Sample-1.png'); background-size: 100% 100%;">
+        <nav class="max-w-screen-2xl mx-auto flex justify-between items-center py-5">
+            <!-- Name Logo Section -->
+            <div>
+                <img src="@/assets/images/Ajith_logo.svg" class="w-11 h-12"/>
+            </div>
 
-        <!-- Content Section -->
-        <div v-if="false" class="max-w-screen-xl h-[500px] flex flex-wrap bg-red-800/30 mx-auto mt-6">
-            <div class="w-1/2 h-full flex flex-col justify-around p-5">
-                <div class="bg-background w-fit py-3 px-5 rounded-lg text-blue-600 font-medium text-bubble">
-                    Hi There!
-                </div>
-                <div>
-                    <div class="text-5xl font-mono">Iam <span class="text-secondary">Ajithkumar</span></div>
+            <!-- Router Hypertext Section -->
+            <div class="flex justify-between flex-wrap gap-10 text-mainText_2">
+                <div v-for="(navLink, idx) in navigationLinks" :key="idx" class="cursor-pointer hover:text-secondary font-medium">
+                    <div>{{ navLink.name }}</div>
                 </div>
             </div>
-            <div class="w-1/2 h-full p-5">
 
-            </div>
-        </div>
+            <!-- Contact Btn Section -->
+            <button class="w-fit h-fit p-2 bg-black rounded-full cursor-pointer transition duration-500 ease-out group hover:bg-secondary hover:ease-in animate-bounce hover:animate-none">
+                <div class="w-fit h-fit px-3 py-1 bg-secondary rounded-full transition duration-500 ease-out group-hover:ease-in group-hover:bg-black">
+                    <span class="text-black font-normal transition duration-500 ease-out group-hover:ease-in group-hover:text-secondary">Contact Me</span>
+                </div>
+            </button>
+        </nav>
     </div>
 </template>
 <script setup lang="ts">
-// ---------- Imports ---------- //
-// import Navigation from '@/pages/Navigation.vue';
+// ---------------- Imports ---------------- //
+
+
+
+// ---------------- Working Variable's ---------------- //
+const navigationLinks = [ // Navigation Links
+    {name:'About Us', link:'#about'},
+    {name:'Products', link:'#about'},
+    {name:'Services', link:'#about'},
+    {name:'Resources', link:'#about'},
+]
+
 </script>
 <style>
 .text-bubble
