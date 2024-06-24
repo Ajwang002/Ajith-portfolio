f5b144e89aaea3f8a20502b14434d3b4b27fc0cc
Ajfury1 <ajfury1@gmail.com>
2024-06-24 08:49:55 +0530

    work pages partially completed

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+const navigationLinks = [
+    {'title': 'Home', href: '/home'},
+    {'title': 'About', href:'/about'},
+    {'title': 'Services', href:'/service'},
+    {'title': 'Works', href:'/work'},
+    {'title': 'Contact', href: '/contacts'},
+]
+
+
+export {
+    navigationLinks,
+}
