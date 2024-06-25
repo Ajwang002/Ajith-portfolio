54fa52f7b9680aa69524da8dcd122171cb1f303d
Ajithkumar Venkatram <121649703+Ajwang002@users.noreply.github.com>
2024-06-25 07:37:50 +0530

    Revert " deploy git script added"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Ajith-portfolio/', // Add your repository name here
   resolve: {
     alias: {
       '@': '/src'
